6e278fe6b6a33be4fddd47142d83b45035595825
JHIH-LEI <vicky88888@gmail.com>
2021-07-28 01:11:08 +0800

    feat: modify dateFormat format

--- tools/helper.js
+++ tools/helper.js
@@ -3,9 +3,9 @@ const functions = {
     const year = date.getFullYear()
     const month = (date.getMonth() + 1)
     const day = date.getDate()
-    const hour = date.getHours()
-    const mins = date.getMinutes()
-    const sec = date.getSeconds()
+    const hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+    const mins = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+    const sec = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
     return `${year}-${month}-${day} ${hour}:${mins}:${sec}`
   }
 }
